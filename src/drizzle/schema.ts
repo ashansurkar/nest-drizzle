08dda3a3745882e379e5ff70eb96cfc5993b83b9
AshAnsurkar <AshishAnsurkar89@gmail.com>
2023-06-14 17:06:44 +1200

    drizzle setup

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -4,7 +4,4 @@ import { relations } from 'drizzle-orm';
 export const users = pgTable('user', {
   user_id: serial('user_id').primaryKey(),
   email: text('email'),
-  aws_cognito_id: text('aws_cognito_id'),
-  role_id: integer('role_id'),
-  employer_id: integer('employer_id'),
 });
